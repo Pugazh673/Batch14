21d8e8b4b1abe9dc187e65406c517aa86e5bf0ae
ADMIN <ADMIN@DESKTOP-K9D2DTS>
2023-08-23 12:07:42 +0530

    This is First commit.

---
+++ StringReverse/src/com/ex/ReverseAString.java
@@ -0,0 +1,18 @@
+package com.ex;
+
+public class ReverseAString {
+
+	public static void main(String[] args) {
+		String word = "Hello";
+		char c;
+		String reverString = "";
+		for (int i = word.length() - 1; i >= 0; i--) {
+			c = word.charAt(i);
+			reverString = reverString + c;
+		}
+		System.out.println(reverString);
+
+		System.out.println(reverString.length());
+	}
+
+}
